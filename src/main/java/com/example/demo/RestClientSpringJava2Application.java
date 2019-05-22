7f82ed8c3a2f81f90dadf612fdddc3fccffe1253
eyvind <eyvind@172.20.200.242>
2019-05-22 15:40:04 +0200

    sdfsf

--- src/main/java/com/example/demo/RestClientSpringJava2Application.java
+++ src/main/java/com/example/demo/RestClientSpringJava2Application.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestClientSpringJava2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestClientSpringJava2Application.class, args);
+                 RestTemplate restTemplate = new RestTemplate();
+                 //omvandla från JSON:
+        Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        String test= quote.toString();
+                   String test2= quote.toString();
 	}
 
 }
